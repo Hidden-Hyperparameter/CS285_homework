391802143ec44ee52a46eb4bb1a35a7f2bcf47e4
ZhaoHanHong <hh-zhao23@mails.tsinghua.edu.cn>
2024-05-23 10:59:22 +0800

    hw1

--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -12,7 +12,10 @@ import gym
 
 import numpy as np
 import torch
-
+import sys
+sys.path.append('.')
+sys.path.append('./cs285')
+sys.path.append('./cs285/infrastructure')
 from cs285.infrastructure import pytorch_util as ptu
 from cs285.infrastructure import utils
 from cs285.infrastructure.logger import Logger
@@ -36,7 +39,6 @@ def run_training_loop(params):
     Args:
         params: experiment parameters
     """
-
     #############
     ## INIT
     #############
@@ -113,6 +115,7 @@ def run_training_loop(params):
     total_envsteps = 0
     start_time = time.time()
 
+    print('parameters:',params)
     for itr in range(params['n_iter']):
         print("\n\n********** Iteration %i ************"%itr)
 
@@ -126,13 +129,14 @@ def run_training_loop(params):
             # BC training from expert data.
             paths = pickle.load(open(params['expert_data'], 'rb'))
             envsteps_this_batch = 0
+            # print('paths',paths)
         else:
             # DAGGER training from sampled data relabeled by expert
             assert params['do_dagger']
             # TODO: collect `params['batch_size']` transitions
             # HINT: use utils.sample_trajectories
             # TODO: implement missing parts of utils.sample_trajectory
-            paths, envsteps_this_batch = TODO
+            paths, envsteps_this_batch = utils.sample_trajectories(env,expert_policy,params['batch_size'],1)
 
             # relabel the collected obs with actions from a provided expert policy
             if params['do_dagger']:
@@ -141,7 +145,7 @@ def run_training_loop(params):
                 # TODO: relabel collected obsevations (from our policy) with labels from expert policy
                 # HINT: query the policy (using the get_action function) with paths[i]["observation"]
                 # and replace paths[i]["action"] with these expert labels
-                paths = TODO
+                paths = None
 
         total_envsteps += envsteps_this_batch
         # add collected data to replay buffer
@@ -151,13 +155,16 @@ def run_training_loop(params):
         print('\nTraining agent using sampled data from replay buffer...')
         training_logs = []
         for _ in range(params['num_agent_train_steps_per_iter']):
-
           # TODO: sample some data from replay_buffer
           # HINT1: how much data = params['train_batch_size']
           # HINT2: use np.random.permutation to sample random indices
           # HINT3: return corresponding data points from each array (i.e., not different indices from each array)
           # for imitation learning, we only need observations and actions.  
-          ob_batch, ac_batch = TODO
+          length = len(replay_buffer.acs)
+          ind = np.random.permutation(length)[:params['train_batch_size']]
+          ac_batch,ob_batch = replay_buffer.acs[ind],replay_buffer.obs[ind]
+          ob_batch = torch.from_numpy(ob_batch).float()
+          ac_batch = torch.from_numpy(ac_batch).float()
 
           # use the sampled data to train an agent
           train_log = actor.update(ob_batch, ac_batch)
