1345d3ea1963fd23b3058a4755568f9a6dcfdecf
ZhaoHanHong <hh-zhao23@tsinghua.edu.cn>
2024-05-26 23:01:58 +0800

    fix plot

--- hw2/report/plot1.py
+++ hw2/report/plot1.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-dic = json.load(open('log.json'))
-ret = np.array(dic['Eval_AverageReturn'])
-ret_var = np.array(dic['Eval_StdReturn'])
-x = np.array(dic['Train_EnvstepsSoFar'])
-# plt.errorbar(x, ret, yerr=ret_var) 
-plt.plot(x,ret)
-plt.xlabel('Train_EnvstepsSoFar')
-plt.ylabel('Eval_AverageReturn')
-# plt.title('Evaluation Average Return with Error Bars')
-plt.show()
+def plot(logname:str):
+    dic = json.load(open(f'{logname}.json'))
+    ret = np.array(dic['Eval_AverageReturn'])
+    ret_var = np.array(dic['Eval_StdReturn'])
+    x = np.array(dic['Train_EnvstepsSoFar'])
+    # plt.errorbar(x, ret, yerr=ret_var) 
+    plt.plot(x,ret,label=logname)
+    plt.xlabel('Train_EnvstepsSoFar')
+    plt.ylabel('Eval_AverageReturn')
+    # plt.title('Evaluation Average Return with Error Bars')
+    # plt.savefig(f'{logname}.png')
+    # plt.close()
