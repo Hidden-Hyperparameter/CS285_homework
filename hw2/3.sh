90e1e4cd09be93c26301151257ca03a95e322d6c
ZhaoHanHong <hh-zhao23@mails.tsinghua.edu.cn>
2024-05-27 19:21:48 +0800

    edit task3 code

--- hw2/3.sh
+++ hw2/3.sh
@@ -1,5 +1,5 @@
 # lambdas=(0 0.95 0.98 0.99 1)
-lambdas=(0.99)
+# lambdas=(0.98)
 
 for lmd in "${lambdas[@]}"
 do
@@ -8,5 +8,5 @@ do
         --discount 0.99 -n 300 -l 3 -s 128 -b 2000 -lr 0.001 \
         --use_reward_to_go --use_baseline --gae_lambda $lmd \
         --exp_name lunar_lander_lambda_$lmd \
-        # > log_lunar_lander_lambda_$lmd.log
+        > log_lunar_lander_lambda_$lmd.log
 done
