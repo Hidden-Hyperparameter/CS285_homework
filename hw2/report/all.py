85000c086fa32277fe2093577e215cbe8fa35659
ZhaoHanHong <hh-zhao23@tsinghua.edu.cn>
2024-05-27 15:20:02 +0800

    fix E1 plot issue

--- hw2/report/all.py
+++ hw2/report/all.py
@@ -2,18 +2,18 @@ from parse_log import load
 from plot1 import plot
 import matplotlib.pyplot as plt
 names = [
-    # 'log_cartpole',
-    # 'log_cartpole_rtg',
-    # 'log_cartpole_na',
-    # 'log_cartpole_rtg_na',
-    'log_cartpole_lb',
-    'log_cartpole_lb_rtg',
-    'log_cartpole_lb_na',
-    'log_cartpole_lb_rtg_na',
+    'log_cartpole',
+    'log_cartpole_rtg',
+    'log_cartpole_na',
+    'log_cartpole_rtg_na',
+    # 'log_cartpole_lb',
+    # 'log_cartpole_lb_rtg',
+    # 'log_cartpole_lb_na',
+    # 'log_cartpole_lb_rtg_na',
 ]
 for name in names:
     load(name)
     plot(name)
     print(f'finish {name}')
 plt.legend()
-plt.savefig('./all_E1-2.png')
+plt.savefig('./all_E1-1.png')
