588b5349a6de8a6d333f3271cc4c13dbf0e306cc
ZhaoHanHong <hh-zhao23@mails.tsinghua.edu.cn>
2024-05-28 09:14:50 +0800

    PGAgent

--- hw2/cs285/agents/pg_agent.py
+++ hw2/cs285/agents/pg_agent.py
@@ -61,26 +61,29 @@ class PGAgent(nn.Module):
         total number of samples across all trajectories (i.e. the sum of the lengths of all the arrays).
         """
 
-        # step 1: calculate Q values of each (s_t, a_t) point, using rewards (r_0, ..., r_t, ..., r_T)
-        # rewards = np.concatenate(tuple(rewards),axis=0)
-        # print(rewards.shape) # [batch,]
+        lens = [len(c) for c in obs]
+        max_len = max(lens)
+        
+        if self.critic is not None:
+            values = [self.critic(ob) for ob in obs]
+        else:
+            values = [torch.zeros([ob.shape[0],1]) for ob in obs]
+
+        values = [np.concatenate((x,np.zeros([max_len-x.shape[0],x.shape[1]])),axis=0) for x in values]
+        actions = [np.concatenate((x,np.zeros([max_len-x.shape[0],x.shape[1]])),axis=0) for x in actions]
+        rewards = [np.concatenate((x,np.zeros([max_len-x.shape[0],x.shape[1]])),axis=0) for x in rewards ]
+        # terminals = [np.concatenate((x,np.zeros([max_len-x.shape[0],x.shape[1]])),axis=0) for x in terminals]
+        values = torch.from_numpy(np.stack(values,axis=0)).to(ptu.device)
+        actions = torch.from_numpy(np.stack(actions,axis=0)).to(ptu.device)
+        rewards = torch.from_numpy(np.stack(rewards,axis=0)).to(ptu.device)
+        # terminals = torch.from_numpy(np.stack(terminals,axis=0)).to(ptu.device)
+
         q_values: Sequence[np.ndarray] = self._calculate_q_vals(rewards)
 
-        # TODO: flatten the lists of arrays into single arrays, so that the rest of the code can be written in a vectorized
-        # way. obs, actions, rewards, terminals, and q_values should all be arrays with a leading dimension of `batch_size`
-        # beyond this point.
-        obs = np.concatenate(obs,axis=0) # [batch,4]
-        actions = np.concatenate(actions,axis=0) # [batch,]
-        terminals = np.concatenate(terminals,axis=0) # [batch,]
-        rewards = np.concatenate(rewards,axis=0) # [batch,]
-
-        # step 2: calculate advantages from Q values
-        advantages: np.ndarray = self._estimate_advantage(
-            obs, rewards, q_values, terminals
+        advantages: torch.Tensor = self._estimate_advantage(
+            rewards, q_values, values
         )
 
-        # step 3: use all datapoints (s_t, a_t, adv_t) to update the PG actor/policy
-        # TODO: update the PG actor/policy network once using the advantages
         info: dict = self.actor.update(
             obs=obs,
             actions=actions,
@@ -88,9 +91,7 @@ class PGAgent(nn.Module):
         )
 
 
-        # step 4: if needed, use all datapoints (s_t, a_t, q_t) to update the PG critic/baseline
         if self.critic is not None:
-            # TODO: perform `self.baseline_gradient_steps` updates to the critic/baseline network
             s=0;n=0
             for _ in range(self.baseline_gradient_steps):
                 s+=self.critic.update(obs,q_values)['Baseline Loss'].item()
@@ -104,81 +105,43 @@ class PGAgent(nn.Module):
     def _calculate_q_vals(self, rewards: Sequence[np.ndarray]) -> Sequence[np.ndarray]:
         """Monte Carlo estimation of the Q function."""
         if not self.use_reward_to_go:
-            # Case 1: in trajectory-based PG, we ignore the timestep and instead use the discounted return for the entire
-            # trajectory at each point.
-            # In other words: Q(s_t, a_t) = sum_{t'=0}^T gamma^t' r_{t'}
-            # TODO: use the helper function self._discounted_return to calculate the Q-values
             q_values = self._discounted_return(rewards)
         else:
-            # Case 2: in reward-to-go PG, we only use the rewards after timestep t to estimate the Q-value for (s_t, a_t).
-            # In other words: Q(s_t, a_t) = sum_{t'=t}^T gamma^(t'-t) * r_{t'}
-            # TODO: use the helper function self._discounted_reward_to_go to calculate the Q-values
             q_values = self._discounted_reward_to_go(rewards)
 
         return q_values
 
     def _estimate_advantage(
         self,
-        obs: np.ndarray,
-        rewards: np.ndarray,
-        q_values: np.ndarray,
-        terminals: np.ndarray,
-    ) -> np.ndarray:
+        rewards: torch.Tensor,
+        q_values: torch.Tensor,
+        values:torch.Tensor
+    ) -> torch.Tensor:
         """Computes advantages by (possibly) subtracting a value baseline from the estimated Q-values.
 
         Operates on flat 1D NumPy arrays.
         """
         if self.critic is None:
-            # TODO: if no baseline, then what are the advantages?
-            # print('TODO: if no baseline, then what are the advantages?')
             advantages = q_values
         else:
-            # TODO: run the critic and use it as a baseline
-            # print('TODO: if no baseline, then what are the advantages?')
-            values = self.critic(obs).detach().cpu().numpy()
             assert values.shape == q_values.shape
 
             if self.gae_lambda is None:
-                # print('if using a baseline, but not GAE, what are the advantages?')
-                # TODO: if using a baseline, but not GAE, what are the advantages?
                 advantages = q_values - values
             else:
-                # TODO: implement GAE
-                batch_size = obs.shape[0]
+                T = rewards.shape[1]-1
 
-                # HINT: append a dummy T+1 value for simpler recursive calculation
-                values = np.append(values, [0])
-                advantages = np.zeros(batch_size + 1)
-                # print('terminals',[x for x in terminals])
-                # deltas = rewards + self.gamma * values[1:]*(1-terminals) - values[:-1]
-                # for i in reversed(range(batch_size)):
-                #     # TODO: recursively compute advantage estimates starting from timestep T.
-                #     # HINT: use terminals to handle edge cases. terminals[i] is 1 if the state is the last in its
-                    # # trajectory, and 0 otherwise.
-                    # advantages[i]=(advantages[i+1]*(self.gae_lambda*self.gamma))*(1-terminals[i])+deltas[i]
-                indices = [i+1 for (i,x) in enumerate(terminals) if x>0.9] # don't off by one!!!
-                indices = [0]+indices+[batch_size]
-                l = []
-                for i,begin_point in enumerate(indices[:-1]):
-                    stop_point = indices[i+1]
-                    leng = stop_point-begin_point
-                    slice_value = values[begin_point:stop_point+1]
-                    slice_value[-1]=0
-                    slice_r = rewards[begin_point:stop_point]
-                    delta = slice_r + self.gamma * slice_value[1:] - slice_value[:-1]
-                    if abs(self.gae_lambda)>1e-4:
-                        reverse_a = np.cumsum(((self.gae_lambda*self.gamma)**(-np.arange(0,leng)))*delta[::-1])
-                        a = (reverse_a*((self.gae_lambda*self.gamma)**(np.arange(0,leng))))[::-1]
-                        l.append(a)
-                    else:
-                        l.append(delta)
-                # remove dummy advantage
-                advantages = np.concatenate(l,axis=0)
-                # advantages = advantages[:-1]
-
-        # TODO: normalize the advantages to have a mean of zero and a standard deviation of one within the batch
+                extended_values = torch.cat(values,torch.zeros([values.shape[0],1]),dim=1)
+                delta = rewards + self.gamma * extended_values[:,1:] - extended_values[:,:-1]
+                if abs(self.gae_lambda)>1e-4:
+                    reverse_a = torch.cumsum(((self.gae_lambda*self.gamma)**(-torch.arange(0,T+1)))*delta[::-1])
+                    advantages = (reverse_a*((self.gae_lambda*self.gamma)**(torch.arange(0,T+1))))[::-1]
+                    
+                else:
+                    advantages = delta
+                
         if self.normalize_advantages:
-            advantages = (advantages - advantages.mean())/advantages.std()
+            advantages = (advantages - advantages.mean(dim=-1,keepdim=True))/advantages.std(dim=-1,keepdim=True)
         return advantages
 
     def _discounted_return(self, rewards: Sequence[float]) -> Sequence[float]:
@@ -189,13 +152,9 @@ class PGAgent(nn.Module):
         Note that all entries of the output list should be the exact same because each sum is from 0 to T (and doesn't
         involve t)!
         """
-        l = []
-        for reward in rewards:
-            T = len(reward)-1
-            gam = self.gamma ** np.arange(0,T+1)
-            l.append(np.ones_like(reward)*np.sum(gam*reward))
-        return np.concatenate(l,axis=0)
-        # return torch.einsum('t,b->bt',np.ones([T+1]),torch.einsum('t,bt->b',gam,rewards)) # batched
+        T = rewards.shape[1]-1
+        gam = self.gamma ** np.arange(0,T+1)
+        return torch.einsum('t,b->bt',np.ones([T+1]),torch.einsum('t,bt->b',gam,rewards)) # batched
 
 
     def _discounted_reward_to_go(self, rewards: Sequence[float]) -> Sequence[float]:
@@ -203,12 +162,7 @@ class PGAgent(nn.Module):
         Helper function which takes a list of rewards {r_0, r_1, ..., r_t', ... r_T} and returns a list where the entry
         in each index t is sum_{t'=t}^T gamma^(t'-t) * r_{t'}.
         """
-        l = []
-        for reward in rewards:
-            T = len(reward)-1
-            gam = self.gamma ** np.arange(0,T+1)
-            mask = (np.arange(0,T+1).reshape(-1,1)>=np.arange(0,T+1).reshape(1,-1)).astype(np.float32) # mask[i,j]=1 iff i>=j
-            ans = np.einsum('pt,t,p->t',mask,gam**-1,gam*reward)
-            l.append(ans)
-        # ans = np.einsum('pt,t,p,bp->bt',mask,gam**-1,gam,rewards) # batched
-        return np.concatenate(l,axis=0)
+        T = rewards.shape[1]-1
+        reverse_a = torch.cumsum((rewards[:,::-1]*(self.gamma**(-torch.arange(0,T+1)))),dim=-1)
+        a = (reverse_a*(self.gamma**(torch.arange(0,T+1))))[:,::-1]
+        return a
